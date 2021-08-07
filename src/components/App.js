2066e114e191f3b373c3b6654ddaed01c715a5d2
Srijan Shovit <srijanshovit@gmail.com>
2021-08-07 15:15:51 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Signup from './Signup';
+import PrivateRoute from './PrivateRoute'
+
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from './Login';
+import ForgotPassword from './ForgotPassword'
+import UpdateProfile from './UpdateProfile'
+
+function App() {
+  //align items for vertical centering
+  //justify content for horizontal centering
+  return (
+    
+      <Container className="d-flex align-items-center justify-content-center" style= {
+    {minHeight: "100vh"}
+  }>
+
+    <div className="w-100" style={{ maxWidth: "400px"}}>
+      <Router>
+        <AuthProvider>
+          <Switch>
+          <PrivateRoute exact path="/" component={Dashboard} />
+          <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+            
+          </Switch>
+
+        </AuthProvider>
+
+      </Router>
+    
+    </div>
+    
+    
+  </Container>
+
+   
+  
+  )
+}
+
+export default App;
